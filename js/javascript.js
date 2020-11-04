d5d7a6712f18916c95abaab2840916b00d4c1395
fatmanur22 <72609984+fatmanur22@users.noreply.github.com>
2020-11-04 16:53:07 +0100

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,24 +1,23 @@
-var knop = document.querySelector('.knoppie');
-console.log("werkt");
+var knop = document.querySelector('header>nav>div>img');
+console.log(knop);
 //  met document.queryselector roep ik de class .knoppie aan, dat is me img op regel 15 van mijn HTML document
 
-var trans = document.querySelector('span');
-console.log("werkt");
+var trans = document.querySelector('header nav div:nth-of-type(2)');
+console.log(trans);
 //  met document.queryselector roep ik de enige span aan die in mijn HTML staat, dat is op regel 23
 
 
 function toggleTrans() {
   trans.classList.toggle('open');
+  console.log("werkt");
 }
-console.log("werkt");
 // als ik op .knoppie druk dan geeft ik mijn span (of te wel var trans, van transform) de class open mee
 // hiermee veranderd de visibility van mijn span van hidden naar visible
 
 
 knop.addEventListener('click', toggleTrans);
-console.log("werkt");
-// eventlistener die er voor zorgt dat als ik op var knop (.knoppie in html) klik mijn span de class open krijgt en zo opent
+console.log(knop);
+// eventlistener die er voor zorgt dat alsik op var knop (.knoppie in html) klik mijn span de class open krijgt en zo opent
 
 trans.addEventListener('click', toggleTrans);
-console.log("werkt");
-// eventlistener: als ik op var trans (span in html) klik, dan haalt hij de class .open weer en sluit dus daardoor
+// eventlistener die er voor zorgt dat alsik op var trans (span in html) klik mijn span de class span krijgt en weer sluit
